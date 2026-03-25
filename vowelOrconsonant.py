e2d7f5be8023665c02fbe2b91ab5cea5a65f24c9
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-03-25 12:33:18 +0530

    Add files via upload

---
+++ vowelOrconsonant.py
@@ -0,0 +1,9 @@
+ch= input("Enetr The Character:::")
+if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print("Character is Vowel")
+elif(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print("Character is Vowel")
+elif(ch=='B' or ch=='C' or ch=='D' or ch=='F' or ch=='G' or ch=='H' or ch=='J' or ch=='K' or ch=='L' or ch=='M' or ch=='N' or ch=='P' or ch=='Q' or ch=='R' or ch=='S' or ch=='T' or ch=='V' or ch=='W' or ch=='X' or ch=='Y' or ch=='Z'):
+    print("Charatcter is Consonant")
+elif(ch=='b' or ch=='c' or ch=='d' or ch=='f' or ch=='g' or ch=='h' or ch=='j' or ch=='k' or ch=='l' or ch=='m' or ch=='n' or ch=='p' or ch=='q' or ch=='r' or ch=='s' or ch=='t' or ch=='v' or ch=='w' or ch=='x' or ch=='y' or ch=='z'):
+    print("Character is Consonant")
