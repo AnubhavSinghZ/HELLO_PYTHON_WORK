e2ba81509af9e2b8afaf09ba8de5340b94081a75
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-04-22 01:13:01 +0530

    Update trafficlight.py

--- trafficlight.py
+++ trafficlight.py
@@ -1,10 +1,10 @@
 light = input("Enter The TRAFFIC LIGHT Color (RED, YELLOW , GREEN):")
 
-if light == "RED":
+if (light=="red") or (light=="RED"):
     print("stop")
-elif light == "YELLOW" :
+elif (light == "YELLOW") or (light=="yellow") :
     print("slow" ) 
-elif light == "GREEN":
+elif (light == "GREEN") or (light=="green"):
     print("go")
 else:
     print("Invalid Input: Please Enter COrrect Light Color")
